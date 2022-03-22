745af7f286410cbb3ab02402b75a0e70f7a8f61f
Peyton <pekellogg@hotmail.com>
2022-03-22 15:14:29 -0700

    commit tree 255963b4ee5fa260290bc1f9dabd1bf6f8d284c9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # FORM CREATE
+  get '/recipes/new' do
+    erb :new
+  end
 
+  # READ ALL
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  # READ SPECIFIC
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  # FORM UPDATE SPECIFIC
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  # FORM UPDATE SPECIFIC
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # CREATE
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  # DELETE
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
 end
